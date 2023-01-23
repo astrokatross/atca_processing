8c9a08ea62405ff827ea67765fc70baf112d2941
astrokatross <kathryn.ross@icrar.org>
2023-01-23 13:55:34 +0800

    🐛 not crash for second band but not fixed

--- measureflux_casa.py
+++ measureflux_casa.py
@@ -1,47 +1,49 @@
 # Script to be run WITHIN CASA!
 day = "day4"
-band="c"
+band="x"
 
-filename = open("/home/cira/ATCA/bin/source_fluxesdict.json", "r")
+
+filename = open("/home/cira/ATCA/bin/data/source_fluxesdict.json", "r")
 srcs = json.load(filename)
 src_names = list(srcs.keys())
 
 
+if band == "c":
+    ms = f"c3487_{day}_cx.ms"
+    spw = "0"
+elif band == "x":
+    ms = f"c3487_{day}_cx.ms"
+    spw = "1"
+elif band == "l":
+    ms = f"c3487_{day}_l.ms"
+    spw = ""
+
+# TODO: add option to check if the band is there not just the file since it crashes for second freq atm
 for key in list(srcs.keys()):
+    # try:
+    #     dict_file = open(f"/home/cira/ATCA/bin/data/{key}_dict.json", "r")
+    #     src_dict = json.load(dict_file)
 
-    if band == "c":
-        ms = f"c3487_{day}_cx.ms"
-        spw = "0"
-    elif band == "x":
-        ms = f"c3487_{day}_cx.ms"
-        spw = "1"
-    elif band == "l":
-        ms = f"c3487_{day}_l.ms"
-        spw = ""
-    try:
-        dict_file = open(f"/home/cira/ATCA/bin/{key}_dict.json", "r")
-        src_dict = json.load(dict_file)
-
-    except:
-        print("Couldn't find the file, refitting the day uv")
-        uvmodelfit(
-            vis=ms,
-            niter=10,
-            field=key,
-            selectdata=True,
-            spw="0",
-            outfile=f"{key}_{band}_{day}.cl"
-        )
-        tbl = cl.open(f"{key}_{band}_{day}.cl")
-        fit = cl.getcomponent(0)
-        flux = fit["flux"]["value"][0]
-        srcs[key][f"{band}"][f"{day}"] = [flux]
-        src_dict = {f"{key}": {f"{band}": {f"{day}": flux}}}
-        # src_dict[f"{key}"][f"{band}"][f"{day}"] = [flux]
+    # except:
+    print("Couldn't find the file, refitting the day uv")
+    uvmodelfit(
+        vis=ms,
+        niter=10,
+        field=key,
+        selectdata=True,
+        spw="0",
+        outfile=f"{key}_{band}_{day}.cl"
+    )
+    tbl = cl.open(f"{key}_{band}_{day}.cl")
+    fit = cl.getcomponent(0)
+    flux = fit["flux"]["value"][0]
+    srcs[key][f"{band}"][f"{day}"] = [flux]
+    src_dict = {f"{key}": {f"{band}": {f"{day}": flux}}}
+    # src_dict[f"{key}"][f"{band}"][f"{day}"] = [flux]
 
 
-        with open(f"/home/cira/ATCA/bin/{key}_dict.json", "w") as f: 
-            json.dump(src_dict,f)
+    with open(f"/home/cira/ATCA/bin/data/{key}_dict.json", "w") as f: 
+        json.dump(src_dict,f)
 
 
 obsinfo = listobs(f"{ms}")
@@ -58,17 +60,16 @@ for key in list(obsinfo.keys()):
         timestamp = obsinfo[key]["0"]["BeginTime"]
         scan = obsinfo[key]["0"]["scanId"]
 
-        dict_file = open(f"/home/cira/ATCA/bin/{fieldname}_dict.json", "r+")
+        dict_file = open(f"/home/cira/ATCA/bin/data/{fieldname}_dict.json", "r+")
         src_dict = json.load(dict_file)
 
         fitflux = src_dict[fieldname][f"{band}"][f"{day}"]
-
         uvmodelfit(
             vis=ms,
             niter=10,
             field=fieldname,
             selectdata=True,
-            spw="0",
+            spw=spw,
             scan = f"{scan}",
             sourcepar = [fitflux, 0, 0],
             outfile=f"{fieldname}_{band}_{day}_{scan}.cl"
@@ -79,7 +80,7 @@ for key in list(obsinfo.keys()):
         flux = fit["flux"]["value"][0]
         src_dict[fieldname][f"{band}"][f"{timestamp}"] = flux
 
-        with open(f"/home/cira/ATCA/bin/{fieldname}_dict.json", "w") as f: 
+        with open(f"/home/cira/ATCA/bin/data/{fieldname}_dict.json", "w") as f: 
             json.dump(src_dict,f)
     else: 
         print("This is not a source I care about apparently!")
